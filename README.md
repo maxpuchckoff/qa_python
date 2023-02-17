ef57bb096157008298762965d58d4409ecd1d2db
Maxim Puchkov <aiki.shaman@gmail.com>
2023-02-17 13:47:40 +0300

    1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# qa_python
+1) test_books_rating_true и test_favourites_true проверяют конструктор _init_
+2) test_add_new_book_add_two_books проверка добавления двух книг
+3) test_add_new_book_new_book_rating_is_one проверка, что добавленной книге присваивается рейтинг по умолчанию равный 1
+4) test_add_new_book_add_the_same_book проверка добавления одной и той же книги дважды
+5) test_set_book_rating_set_rating_to_book_not_in_list проверка что нельзя присвоить рейтинг книге, которой нет в списке
+6) test_set_book_rating_set_book_rating_less_1 проверка, что нельзя выставить рейтинг меньше 1
+7) test_set_book_rating_set_book_rating_more_10 проверка, что нельзя выставить рейтинг больше 10
+8) test_get_book_rating_true проверка получения рейтинга по названию книги
+9) test_get_books_with_specific_rating_true проверка получения списка книг с определенным рейтингом
+10) test_get_books_rating_true проверка получения спика книг с рейтингом
+11) test_add_book_in_favorites_true проверка добавления книги в избранное
+12) test_delete_book_from_favorites_true проверка удаления книги из избранного
+13) test_get_list_of_favorites_books_true проверка получения списка избранных книг
--- tests.py
+++ tests.py
@@ -1,24 +1,83 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
-
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    def test_books_rating_true(self):
         collector = BooksCollector()
+        assert collector.books_rating == {}
 
-        # добавляем две книги
+    def test_favourites_true(self):
+        collector = BooksCollector()
+        assert collector.favorites == []
+
+    def test_add_new_book_add_two_books(self):
+        collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_new_book_rating_is_one(self):
+        collector = BooksCollector()
+        collector.add_new_book('Sapiens:краткая история человечества')
+        assert collector.books_rating['Sapiens:краткая история человечества'] == 1
+
+    def test_add_new_book_add_the_same_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Sapiens:краткая история человечества')
+        collector.add_new_book('Sapiens:краткая история человечества')
+        assert len(collector.get_books_rating()) == 1
+    def test_set_book_rating_set_rating_to_book_not_in_list(self):
+        collector = BooksCollector()
+        collector.set_book_rating('Переписка Энгельса с Каутским', 2)
+        assert collector.get_book_rating('Переписка Энгельса с Каутским') == None
+
+    def test_set_book_rating_set_book_rating_less_1(self):
+        collector = BooksCollector()
+        collector.add_new_book('Переписка Энгельса с Каутским')
+        collector.set_book_rating('Переписка Энгельса с Каутским', 0)
+        assert collector.get_book_rating('Переписка Энгельса с Каутским') > 0
+
+    def test_set_book_rating_set_book_rating_more_10(self):
+        collector = BooksCollector()
+        collector.add_new_book('Sapiens:краткая история человечества')
+        collector.set_book_rating('Sapiens:краткая история человечества', 15)
+        assert collector.get_book_rating('Sapiens:краткая история человечества') < 11
+
+    def test_get_book_rating_true(self):
+        collector = BooksCollector()
+        collector.add_new_book('Тестирование Дот Ком')
+        assert collector.get_book_rating('Тестирование Дот Ком') == 1
+
+    def test_get_books_with_specific_rating_true(self):
+        collector = BooksCollector()
+        collector.books_rating = {'Мастер и Маргарита': 8, 'Sapiens:краткая история человечества': 8,
+                                  'Переписка Энегельса с Каутским': 2}
+        assert collector.get_books_with_specific_rating(8) == ['Мастер и Маргарита',
+                                                               'Sapiens:краткая история человечества']
+
+    def test_get_books_rating_true(self):
+        collector = BooksCollector()
+        collector.books_rating = {'Мастер и Маргарита': 8, 'Sapiens:краткая история человечества': 8,
+                                  'Переписка Энегельса с Каутским': 2}
+        assert collector.get_books_rating() == {'Мастер и Маргарита': 8, 'Sapiens:краткая история человечества': 8,
+                                  'Переписка Энегельса с Каутским': 2}
+
+    def test_add_book_in_favorites_true(self):
+        collector = BooksCollector()
+        collector.books_rating = {'Сага о ведьмаке': 10}
+        collector.add_book_in_favorites('Сага о ведьмаке')
+        assert collector.favorites == ['Сага о ведьмаке']
+
+    def test_delete_book_from_favorites_true(self):
+        collector = BooksCollector()
+        collector.favorites = ['Сага о ведьмаке', 'Переписка Энгельса с Каутским']
+        collector.delete_book_from_favorites('Переписка Энгельса с Каутским')
+        assert collector.favorites == ['Сага о ведьмаке']
+
+    def test_get_list_of_favorites_books_true(self):
+        collector = BooksCollector()
+        collector.favorites = ['Сага о ведьмаке', 'Переписка Энгельса с Каутским']
+        assert collector.get_list_of_favorites_books() == ['Сага о ведьмаке', 'Переписка Энгельса с Каутским']
+
+
+
+
